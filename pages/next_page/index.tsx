9831d4acb5ef9faf597c811196ee93b7ce07b64f
Joe King <joeking@Joes-Air.lan>
2023-03-24 23:55:21 -0500

    I don't remember but it's not important

--- pages/next_page/index.tsx
+++ pages/next_page/index.tsx
@@ -1,13 +1,18 @@
 
+import styles from "@/styles/Home.module.css";
 
 export default function NextPage() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-0">
-        <h1>Heading One</h1>
-        <h1>Heading Two</h1>
-        <h1>Heading Three</h1>
-      </div>
+      <main>
+        <div className={styles.main}>
+          <div className="grid grid-cols-3">
+            <h1>Heading One</h1>
+            <h1>Heading Two</h1>
+            <h1>Heading Three</h1>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
